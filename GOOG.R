1a67391119aac5f2940b983c30d1261cb0d46ec8
gaoyifan99 <71992455+gaoyifan99@users.noreply.github.com>
2020-11-26 15:36:53 +0800

    Update GOOG.R

--- GOOG.R
+++ GOOG.R
@@ -1,14 +1,14 @@
 #COV-19 DATA
 library(lubridate)
 data<-read.csv("https://raw.githubusercontent.com/gaoyifan99/COV-19-Project/master/national-history.csv",header=TRUE,sep=",",quote="")
-data$date<-lubridate::ymd(data$date) 
+
 
 
 #stock date
 stock_ymd<-read.csv("https://raw.githubusercontent.com/gaoyifan99/COV-19-Project/master/GOOG.csv",header=TRUE,sep=",",quote="")
-stock_ymd$date<-lubridate::ymd(stock_ymd$date)
 
-# 数据关联
+
+# 脢媒戮脻鹿脴脕陋
 library(dplyr)
 covid_stock_join<-dplyr::left_join(data,stock_ymd,by="date")
 
