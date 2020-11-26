14de2e4fe2cf2e0527ca30c27c4c762b005b900a
gaoyifan99 <71992455+gaoyifan99@users.noreply.github.com>
2020-11-26 15:37:55 +0800

    Update GOOG.R

--- GOOG.R
+++ GOOG.R
@@ -8,9 +8,6 @@ data<-read.csv("https://raw.githubusercontent.com/gaoyifan99/COV-19-Project/mast
 stock_ymd<-read.csv("https://raw.githubusercontent.com/gaoyifan99/COV-19-Project/master/GOOG.csv",header=TRUE,sep=",",quote="")
 
 
-# Êý¾Ý¹ØÁª
-library(dplyr)
-covid_stock_join<-dplyr::left_join(data,stock_ymd,by="date")
 
 
 
